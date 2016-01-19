9afc6d16113c1a4dc3569d132b1b880fb3409f51
Karol Suwalski <[suwalski.karol@gmail.com]>
2016-01-19 02:02:02 +0100

    Quartz

---
+++ Lab 5. - Zadania okresowe/Scheduler/QuartzWithListeners/ExampleJob.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Quartz;
+
+namespace QuartzWithListeners
+{
+    class ExampleJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("--> Executing the example job");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Lab 5. - Zadania okresowe/Scheduler/QuartzWithListeners/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Impl.Matchers;
+using QuartzWithListeners.Listeners;
+
+namespace QuartzWithListeners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scheduler = new StdSchedulerFactory().GetScheduler();
+
+            // add scheduler listener
+            scheduler.ListenerManager.AddSchedulerListener(new SchedulerListener());
+
+            // add global job listener
+            scheduler.ListenerManager.AddJobListener(new JobListener(), GroupMatcher<JobKey>.AnyGroup());
+
+            // add global trigger listener
+            scheduler.ListenerManager.AddTriggerListener(new TriggerListener(), GroupMatcher<TriggerKey>.AnyGroup());
+
+            // add jobs & triggers
+            scheduler.ScheduleJob(
+                JobBuilder.Create<ExampleJob>().WithIdentity("MyJob", "MyJobGroup").Build(),
+                TriggerBuilder.Create().WithIdentity("MyTrigger", "MyTriggerGroup")
+                    .WithSimpleSchedule(s => s.WithIntervalInSeconds(2).RepeatForever()).Build());
+
+
+            scheduler.Start(); // start scheduler
+            Thread.Sleep(5000); // sleep the main thread (Quartz will fire the job 3x on other threads)
+            scheduler.Shutdown(true); // stop the scheduler
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
